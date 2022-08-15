1a389fa219776cf6c9a9e6c55b76fd89b3c8f55f
Sandeep <gunjalsandeep21@gmail.com>
2022-08-15 11:12:56 +0530

    This is my first github push.

---
+++ test.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://gunjalsandeep21:rockstar21@sandeep.as3e6al.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
